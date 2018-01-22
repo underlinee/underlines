44ee71cde45c6a8f892995f29f71a6d03beecd2b
underlinee <jb.squee@daum.net>
2018-01-22 19:22:41 +0900

    Added function to build word2vec model

--- src/test/test_trainer.py
+++ src/test/test_trainer.py
@@ -1,13 +1,32 @@
+import os
 from unittest import TestCase
 
 from underlines import trainer
-
-
-text = """인간의 왕조가 흥망성쇠를 거듭하는 동안 이 작은 씨앗은 미래에 대한 희망을 버리지 않고 고집스럽게 버틴 것이다.    
+from underlines.common import config
+sentences = ["""인간의 왕조가 흥망성쇠를 거듭하는 동안 이 작은 씨앗은 미래에 대한 희망을 버리지 않고 고집스럽게 버틴 것이다.    
                     그러다가 어느 날 그 작은 식물의 열망이 어느 실험실 안에서 활짝 피었다. 그 연꽃은 지금 어디 있을까. 모든 시작은 기다림의 끝이다.
                     우리는 모두 단 한 번의 기회를 만난다. 우리는 모두 한 사람 한 사람 불가능하면서도 필연적인 존재들이다. 
-                    모든 우거진 나무의 시작은 기다림을 포기하지 않은 씨앗이었다."""
+                    모든 우거진 나무의 시작은 기다림을 포기하지 않은 씨앗이었다.""",
+        """위쪽에서는 개울도 숨을 죽이고 있었다. 바위 사이로 사라져 지하로 흘러가는 지점이었다. 
+        훨씬 낮은 음으로 분지에 불어오는 바람 소리가 들리기 시작했다. 호수는 움직이는 밤하늘이었다. 바람은 잔물결을 이쪽에서 저쪽 해안으로 밀어냈고, 
+        까만 물 위의 물결을 따라 펼쳐진 별빛은 사라지고 나타나기를 반복하다가 갑작스레 방향을 바꾸곤 했다.나는 그대로 멈춰서 그 그림같은 광경을 바라보았다. 
+        인간이 없을 때의 산의 삶을 체험한 것 같았다. 나는 방해가 되지 않게 있었고 거기서 제법 환영받는 손님이었다. 이런 산이 나와 함께 있어준다면 외롭지 않을 거라고 다시금 깨닫게 되었다."""]
+
+MODEL_PATH = config.get("MODEL_PATH")
 
 class TestTrain(TestCase):
     def test_train(self):
-        trainer.train(text)
+        if not os.path.exists(MODEL_PATH):
+            trainer.build(sentences)
+        trainer.train(sentences)
+
+    def test_build(self):
+        if os.path.exists(MODEL_PATH):
+            os.remove(MODEL_PATH)
+        trainer.build(sentences)
+
+    def test_is_valid_token_invalid(self):
+        assert trainer._is_valid_token(("TEST", "INVALID")) == False
+
+    def test_is_valid_token(self):
+        assert trainer._is_valid_token(("여사", "Noun")) == True
--- src/underlines/trainer.py
+++ src/underlines/trainer.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
-import json
 
-from os import path
+import json
 from gensim.models import word2vec
+
+from underlines.common import config
 
 STOPWORDS = json.load(open('../../resources/stopwords.json', 'rt', encoding='utf-8'))
 VAILD_POS = ("Noun", "Adjective", "Verb")
-MODEL_PATH='../../resources/underlines.model'
+MODEL_PATH = config.get("MODEL_PATH")
 
-def train(txts):
-    if path.exists(MODEL_PATH):
-        model = word2vec.Word2Vec.load(MODEL_PATH)
-        model.init_sims(replace=True)
-        model.save(MODEL_PATH)
-    else:
-        create_model(txts)
-
-def create_model(txts):
-    model = word2vec.Word2Vec(_tokenize(txts), size=50, window=2, min_count=1, workers=4, iter=100, sg=1)
+def train(sentences):
+    model = word2vec.Word2Vec.load(MODEL_PATH)
+    list_of_tokens = _tokenize(sentences)
+    total_words = sum(len(tokens) for tokens in list_of_tokens)
+    model.train(list_of_tokens, total_examples=len(sentences), total_words=total_words, epochs=50)
     model.init_sims(replace=True)
     model.save(MODEL_PATH)
 
-def _tokenize(txts):
+def build(sentences):
+    # if not os.path.exists(MODEL_PATH):
+    model = word2vec.Word2Vec(_tokenize(sentences), size=100, window=3, min_count=1, workers=4, iter=50, sg=1)
+    model.init_sims(replace=True)
+    model.save(MODEL_PATH)
+
+def _tokenize(sentences):
     from konlpy.tag import Twitter
     twitter = Twitter()
-    pos = lambda txt: ['/'.join(token) for token in twitter.pos(txt, True, True) if _is_valid_token(token)]
-    tokens = [pos(txt) for txt in txts]
+    pos = lambda sentence: ['/'.join(token) for token in twitter.pos(sentence, True, True) if _is_valid_token(token)]
+    tokens = [pos(sentence) for sentence in sentences]
     return tokens
 
 def _is_valid_token(token):
